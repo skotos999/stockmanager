64f72da470e12c0920d8e079543c16b4e6887852
X30 <x3cut0r@mailbox.hu>
2014-04-05 11:42:58 +0200

    fixed mainwindow to show up correctly

--- src/main/java/stockmanager/ui/MainWindow.java
+++ src/main/java/stockmanager/ui/MainWindow.java
@@ -2,17 +2,20 @@ package stockmanager.ui;
 
 import javax.swing.*;
 
-public class MainWindow  extends JFrame {
+public class MainWindow extends JFrame {
     public static final String APPLICATION_TITLE = "Sheriff's Office";
     public static final String MAIN_WINDOW_NAME = "Animal Breeding";
     private JTextField textField1;
     private JButton calculateButton;
+    private JPanel mainPanel;
 
-    public MainWindow(){
+    public MainWindow() {
         setName(MainWindow.MAIN_WINDOW_NAME);
+        setContentPane(mainPanel);
         setTitle(APPLICATION_TITLE);
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
+
 }
